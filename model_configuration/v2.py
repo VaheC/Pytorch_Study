826cb7a368837738de1ce46d7d6c688581923ee9
VaheC <vcharchian@gmail.com>
2023-07-22 18:16:09 +0400

    model configuration v2 added

---
+++ model_configuration/v2.py
@@ -0,0 +1,16 @@
+
+lr = 0.1
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+torch.manual_seed(42)
+
+model = nn.Sequential(nn.Linear(1, 1)).to(device)
+
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+lossfn = nn.MSELoss(reduction='mean')
+
+train_step_fn = make_train_step_fn(model, optimizer, lossfn)
+
+val_step_fn = make_val_step_fn(model, lossfn)
