bf67ec2586ddd5352b7de947046e9456a6b18314
VaheC <vcharchian@gmail.com>
2023-07-29 10:25:39 +0400

    stepbystep v3 added

---
+++ stepbystep/v3.py
@@ -0,0 +1,426 @@
+
+import numpy as np
+import random
+
+import datetime
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset, random_split
+import torch.functional as F
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.transforms import Normalize
+
+import matplotlib.pyplot as plt
+# %matplotlib inline
+plt.style.use('fivethirtyeight')
+
+class StepByStep(object):
+    
+    def __init__(self, model, loss_fn, optimizer):
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model.to(self.device)
+
+        self.train_loader = None
+        self.valid_loader = None
+        self.writer = None
+
+        self.losses = []
+        self.valid_losses = []
+        self.total_epochs = 0
+
+        self.train_step_fn = self._make_train_step_fn()
+        self.valid_step_fn = self._make_valid_step_fn()
+
+    def to(self, device):
+        try:
+            self.device = device
+            self.model.to(self.device)
+        except RuntimeError:
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+            print(f"{device} not available, sending to {self.device}")
+            self.model.to(self.device)
+
+    def set_loaders(self, train_loader, valid_loader=None):
+        self.train_loader = train_loader
+        self.valid_loader = valid_loader
+
+    def set_tensorboard(self, name, folder='runs'):
+        suffix = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        self.writer = SummaryWriter(f'{folder}/{name}_{suffix}')
+
+    def _make_train_step_fn(self):
+
+        def perform_train_step(x, y):
+
+            self.model.train()
+
+            y_hat = self.model(x)
+
+            loss = self.loss_fn(y_hat, y)
+
+            loss.backward()
+
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            return loss.item()
+        
+        return perform_train_step
+
+    def _make_valid_step_fn(self):
+
+        def perform_val_step(x, y):
+
+            self.model.eval()
+
+            y_hat = self.model(x)
+
+            loss = self.loss_fn(y_hat, y)
+
+            return loss.item()
+        
+        return perform_val_step
+    
+    def _get_minibatch_loss(self, validation=False):
+
+        if validation:
+            step_fn = self.valid_step_fn
+            data_loader = self.valid_loader
+        else:
+            step_fn = self.train_step_fn
+            data_loader = self.train_loader
+
+        if data_loader is None:
+            return None
+
+        mini_batch_losses = []
+
+        for x_batch, y_batch in data_loader:
+
+            x_batch = x_batch.to(self.device)
+            y_batch = y_batch.to(self.device)
+
+            mini_batch_losses.append(step_fn(x_batch, y_batch))
+
+        loss = np.mean(mini_batch_losses)
+        
+        return loss
+    
+    def set_seed(self, seed=42):
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        random.seed(seed)
+        try:
+            self.train_loader.sampler.generator.manual_seed(seed)
+        except AttributeError:
+            pass
+
+    def train(self, epochs, seed=42):
+
+        self.set_seed(seed)
+
+        for epoch in range(epochs):
+
+            self.total_epochs += 1
+
+            loss = self._get_minibatch_loss(validation=False)
+            self.losses.append(loss)
+
+            with torch.no_grad():
+                valid_loss = self._get_minibatch_loss(validation=True)
+                self.valid_losses.append(valid_loss)
+
+            if self.writer:
+                scalars = {'training': loss}
+
+                if valid_loss is not None:
+                    scalars.update({'validation': valid_loss})
+
+                self.writer.add_scalars(
+                    main_tag='loss',
+                    tag_scalar_dict=scalars,
+                    global_step=epoch
+                )
+
+        if self.writer:
+            self.writer.flush()
+
+    def save_checkpoint(self, filename):
+        checkpoint = {
+            'epoch': self.total_epochs,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'loss': self.losses,
+            'val_loss': self.valid_losses
+        }
+
+        torch.save(checkpoint, filename)
+
+    def load_checkpoint(self, filename):
+        checkpoint = torch.load(filename)
+
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+        self.total_epochs = checkpoint['epoch']
+        self.losses = checkpoint['loss']
+        self.valid_losses = checkpoint['val_loss']
+
+        self.model.train()
+
+    def predict(self, x):
+        self.model.eval()
+        x_tensor = torch.as_tensor(x).float()
+        y_hat_tensor = self.model(x_tensor.to(self.device))
+        self.model.train()
+        return y_hat_tensor.detach().cpu().numpy()
+    
+    def plot_losses(self):
+        fig = plt.figure(figsize=(10, 4))
+        plt.plot(self.losses, label='Training Loss', c='b')
+        if self.valid_loader:
+            plt.plot(self.valid_losses, label='Validation Loss', c='r')
+        plt.yscale('log')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.tight_layout()
+        return fig
+    
+    def add_graph(self):
+        if self.train_loader and self.writer:
+            x_dummy, y_dummy = next(iter(self.train_loader))
+            self.writer.add_graph(self.model, x_dummy.to(self.device))
+
+    def count_parameters(self):
+        return sum(p.numel for p in self.model.parameters() if p.requires_grad)
+
+    @staticmethod
+    def _visualize_tensors(axs, x, y=None, yhat=None,
+        layer_name='', title=None):
+        # The number of images is the number of subplots in a row
+        n_images = len(axs)
+        # Gets max and min values for scaling the grayscale
+        minv, maxv = np.min(x[:n_images]), np.max(x[:n_images])
+        # For each image
+        for j, image in enumerate(x[:n_images]):
+            ax = axs[j]
+            # Sets title, labels, and removes ticks
+            if title is not None:
+                ax.set_title(f'{title} #{j}', fontsize=12)
+            shp = np.atleast_2d(image).shape
+            ax.set_ylabel(
+            f'{layer_name}\n{shp[0]}x{shp[1]}',
+            rotation=0, labelpad=40
+            )
+            xlabel1 = '' if y is None else f'\nLabel: {y[j]}'
+            xlabel2 = '' if yhat is None else f'\nPredicted: {yhat[j]}'
+            xlabel = f'{xlabel1}{xlabel2}'
+            if len(xlabel):
+                ax.set_xlabel(xlabel, fontsize=12)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            # Plots weight as an image
+            ax.imshow(
+            np.atleast_2d(image.squeeze()),
+            cmap='gray',
+            vmin=minv,
+            vmax=maxv
+            )
+        return
+    
+    def visualize_filters(self, layer_name, **kwargs):
+        try:
+            # Gets the layer object from the model
+            layer = self.model
+            for name in layer_name.split('.'):
+                layer = getattr(layer, name)
+            # We are only looking at filters for 2D convolutions
+            if isinstance(layer, nn.Conv2d):
+                # Takes the weight information
+                weights = layer.weight.data.cpu().numpy()
+                # weights -> (channels_out (filter), channels_in, H, W)
+                n_filters, n_channels, _, _ = weights.shape
+                # Builds a figure
+                size = (2 * n_channels + 2, 2 * n_filters)
+                fig, axes = plt.subplots(n_filters, n_channels,
+                figsize=size)
+                axes = np.atleast_2d(axes)
+                axes = axes.reshape(n_filters, n_channels)
+                # For each channel_out (filter)
+                for i in range(n_filters):
+                    StepByStep._visualize_tensors(
+                    axes[i, :],
+                    weights[i],
+                    layer_name=f'Filter #{i}',
+                    title='Channel'
+                    )
+                for ax in axes.flat:
+                    ax.label_outer()
+                fig.tight_layout()
+                return fig
+        except AttributeError:
+            return
+        
+    def attach_hooks(self, layers_to_hook, hook_fn=None):
+        # Clear any previous values
+        self.visualization = {}
+        # Creates the dictionary to map layer objects to their names
+        modules = list(self.model.named_modules())
+        layer_names = {layer: name for name, layer in modules[1:]}
+        self.handles = {}
+
+        if hook_fn is None:
+            # Hook function to be attached to the forward pass
+            def hook_fn(layer, inputs, outputs):
+                # Gets the layer name
+                name = layer_names[layer]
+                # Detaches outputs
+                values = outputs.detach().cpu().numpy()
+                # Since the hook function may be called multiple times
+                # for example, if we make predictions for multiple
+                # mini-batches, it concatenates the results
+                if self.visualization[name] is None:
+                    self.visualization[name] = values
+                else:
+                    self.visualization[name] = np.concatenate([self.visualization[name], values])
+
+        for name, layer in modules:
+            # If the layer is in our list
+            if name in layers_to_hook:
+                # Initializes the corresponding key in the dictionary
+                self.visualization[name] = None
+                # Register the forward hook and keep the handle
+                # in another dict
+                self.handles[name] = layer.register_forward_hook(hook_fn)
+
+    def remove_hooks(self):
+        # Loops through all hooks and removes them
+        for handle in self.handles.values():
+            handle.remove()
+        # Clear the dict, as all hooks have been removed
+        self.handles = {}
+
+    def visualize_outputs(self, layers, n_images=10, y=None, yhat=None):
+        layers = filter(lambda l: l in self.visualization.keys(), layers)
+        layers = list(layers)
+        shapes = [self.visualization[layer].shape for layer in layers]
+        n_rows = [shape[1] if len(shape) == 4 else 1 for shape in shapes]
+        total_rows = np.sum(n_rows)
+        fig, axes = plt.subplots(total_rows, n_images,
+        figsize=(1.5*n_images, 1.5*total_rows))
+        axes = np.atleast_2d(axes).reshape(total_rows, n_images)
+        # Loops through the layers, one layer per row of subplots
+        row = 0
+
+        for i, layer in enumerate(layers):
+            start_row = row
+            # Takes the produced feature maps for that layer
+            output = self.visualization[layer]
+            is_vector = len(output.shape) == 2
+
+            for j in range(n_rows[i]):
+                StepByStep._visualize_tensors(
+                axes[row, :],
+                output if is_vector else output[:, j].squeeze(),
+                y,
+                yhat,
+                layer_name=layers[i] \
+                if is_vector \
+                else f'{layers[i]}\nfil#{row-start_row}',
+                title='Image' if (row == 0) else None
+                )
+                row += 1
+
+        for ax in axes.flat:
+            ax.label_outer()
+        plt.tight_layout()
+
+        return fig
+    
+    def correct(self, x, y, threshold=.5):
+        self.model.eval()
+        yhat = self.model(x.to(self.device))
+        y = y.to(self.device)
+        self.model.train()
+        # We get the size of the batch and the number of classes
+        # (only 1, if it is binary)
+        n_samples, n_dims = yhat.shape
+
+        if n_dims > 1:
+            # In a multiclass classification, the largest logit
+            # always wins, so we don't bother getting probabilities
+            # This is PyTorch's version of argmax,
+            # but it returns a tuple: (max value, index of max value)
+            _, predicted = torch.max(yhat, 1)
+        else:
+            n_dims += 1
+            # In binary classification, we NEED to check if the
+            # last layer is a sigmoid (and then it produces probs)
+            if isinstance(self.model, nn.Sequential) and \
+                isinstance(self.model[-1], nn.Sigmoid):
+                predicted = (yhat > threshold).long()
+            # or something else (logits), which we need to convert
+            # using a sigmoid
+            else:
+                predicted = (torch.sigmoid(yhat) > threshold).long()
+
+        # How many samples got classified correctly for each class
+        result = []
+        for c in range(n_dims):
+            n_class = (y == c).sum().item()
+            n_correct = (predicted[y == c] == c).sum().item()
+            result.append((n_correct, n_class))
+        return torch.tensor(result)
+    
+    @staticmethod
+    def loader_apply(loader, func, reduce='sum'):
+        results = [func(x, y) for i, (x, y) in enumerate(loader)]
+        results = torch.stack(results, axis=0)
+        if reduce == 'sum':
+            results = results.sum(axis=0)
+        elif reduce == 'mean':
+            results = results.float().mean(axis=0)
+        return results
+
+    @staticmethod
+    def statistics_per_channel(images, labels):
+        # NCHW
+        n_samples, n_channels, n_height, n_weight = images.size()
+        # Flatten HW into a single dimension
+        flatten_per_channel = images.reshape(n_samples, n_channels, -1)
+        # Computes statistics of each image per channel
+        # Average pixel value per channel
+        # (n_samples, n_channels)
+        means = flatten_per_channel.mean(axis=2)
+        # Standard deviation of pixel values per channel
+        # (n_samples, n_channels)
+        stds = flatten_per_channel.std(axis=2)
+        # Adds up statistics of all images in a mini-batch
+        # (1, n_channels)
+        sum_means = means.sum(axis=0)
+        sum_stds = stds.sum(axis=0)
+        # Makes a tensor of shape (1, n_channels)
+        # with the number of samples in the mini-batch
+        n_samples = torch.tensor([n_samples]*n_channels).float()
+        # Stack the three tensors on top of one another
+        # (3, n_channels)
+        return torch.stack([n_samples, sum_means, sum_stds], axis=0)
+    
+    @staticmethod
+    def make_normalizer(loader):
+        total_samples, total_means, total_stds = \
+        StepByStep.loader_apply(
+        loader,
+        StepByStep.statistics_per_channel
+        )
+        norm_mean = total_means / total_samples
+        norm_std = total_stds / total_samples
+        return Normalize(mean=norm_mean, std=norm_std)
